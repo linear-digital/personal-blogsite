95b10753c21503c10989ba4f13e5135323fce2b6
Linear Digital (Tamiz) <115143466+linear-digital@users.noreply.github.com>
2025-02-25 02:01:55 +0600

    Update robots.js

--- app/robots.js
+++ app/robots.js
@@ -1,10 +1,16 @@
 
 export default function robots() {
-  return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://genzit.xyz/sitemap.xml",
-  };
+    return {
+        rules: [
+            {
+                userAgent: "Googlebot",
+                allow: "/"
+            },
+            {
+                userAgent: "*",
+                allow: "/"
+            },
+        ],
+        sitemap: "https://genzit.xyz/sitemap.xml",
+    };
 }
